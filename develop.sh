6650c85e64559a35171efed13ffde287198c84da
markosluga <35494371+markosluga@users.noreply.github.com>
2021-10-22 14:00:14 -0400

    Update develop.sh

--- develop.sh
+++ develop.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo "Configuring kolla"
-source kolla-config.sh
-echo "Deploying kolla-single host"
+echo "Configuring and deploying kolla-single host"
 source kolla-maker.sh
 echo "Running post install"
 source kolla-post-install.sh
