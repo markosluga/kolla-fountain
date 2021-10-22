d2caa8eadf105614cb7d498dbed4ef65e992f4be
kolla <kolla@singlet.mshome.net>
2021-10-22 17:45:21 +0000

    fixed hosts

--- host-prep.sh
+++ host-prep.sh
@@ -2,7 +2,6 @@
 # Make kolla user a sudoer
 sudo sed -i -e '$akolla ALL=(ALL) NOPASSWD: ALL' /etc/sudoers
 # Change the hosts entry to match to the kolla_internal_vip_address 
-sudo sed -i '/127.0.0.1 localhost/d' /etc/hosts
-sudo sed -i '/127.0.0.1 singlet/d' /etc/hosts
+sudo sed -i '1,2d' /etc/hosts
 sudo pvcreate /dev/sdb
 sudo vgcreate cinder-volumes /dev/sdb
