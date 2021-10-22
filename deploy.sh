c9b0006c17a9150602a476839116f93a6dc86841
markosluga <35494371+markosluga@users.noreply.github.com>
2021-10-21 21:19:38 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+echo "Prepping host"
 source host-prep.sh
+echo "Installing dependecies and configureing kolla"
 source kolla-shaker.sh
+echo "Deploying kolla-single host"
 source kolla-maker.sh
+echo "Running post install"
+source kolla-post-install.sh
+echo "Process completed successfully!"
+echo "OpenStack deployed at 172.25.216.200"
