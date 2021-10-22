6349a4893a3a1e79cf54e272257b005ff58fecc3
markosluga <35494371+markosluga@users.noreply.github.com>
2021-10-22 09:40:54 -0400

    Update kolla-config.sh

--- kolla-config.sh
+++ kolla-config.sh
@@ -21,9 +21,16 @@ sudo sed -i "44i enable_nova: \"{{ enable_openstack_core | bool }}\" " /etc/koll
 sudo sed -i "45i nova_compute_virt_type:  \"qemu\" " /etc/kolla/globals.yml
 sudo sed -i "46i enable_neutron_provider_networks: \"yes\" " /etc/kolla/globals.yml
 sudo sed -i "47i enable_neutron_port_forwarding: \"yes\" " /etc/kolla/globals.yml
-sudo sed -i "48i enable_openvswitch: \"yes\" " /etc/kolla/globals.yml
+sudo sed -i "48i enable_openvswitch: \"yes\" " /etc/kolla/globals.yml 
+
 # Add the OVS mappings
 sudo echo "[ml2_type_flat]" >>  /etc/kolla/config/neutron/ml2_conf.ini
 sudo echo "flat_networks = physnet1,physnet2" >>  /etc/kolla/config/neutron/ml2_conf.ini
-sudo echo "[ovs]" >>  /etc/kolla/config/neutron/ml2_conf.ini
-sudo echo "bridge_mappings = physnet1:brex,physnet2:brex2" >>  /etc/kolla/config/neutron/ml2_conf.ini
+#sudo echo "[ovs]" >>  /etc/kolla/config/neutron/ml2_conf.ini
+#sudo echo "bridge_mappings = physnet1:brex,physnet2:brex2" >>  /etc/kolla/config/neutron/ml2_conf.ini
+sudo echo "[ovs]" >> /etc/kolla/config/neutron/openvswitch_agent.ini
+sudo echo "tunnel_bridge = br-tun" >> /etc/kolla/config/neutron/openvswitch_agent.ini
+sudo echo "int_peer_patch_port = patch-tun" >> /etc/kolla/config/neutron/openvswitch_agent.ini
+sudo echo "tun_peer_patch_port = patch-int" >> /etc/kolla/config/neutron/openvswitch_agent.ini
+sudo echo "bridge_mappings = physnet1:brex,physnet2:brex2" >> /etc/kolla/config/neutron/openvswitch_agent.ini
+sudo echo "use_veth_interconnection = false" >> /etc/kolla/config/neutron/openvswitch_agent.ini
