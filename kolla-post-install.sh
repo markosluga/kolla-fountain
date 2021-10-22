136e097b58425b0da41a39c3b0893ee58cde645f
markosluga <35494371+markosluga@users.noreply.github.com>
2021-10-22 01:44:27 -0400

    Update kolla-post-install.sh

--- kolla-post-install.sh
+++ kolla-post-install.sh
@@ -2,9 +2,9 @@
 sudo apt install -y openvswitch-switch python3-openstackclient
 kolla-ansible post-deploy
 . /etc/kolla/admin-openrc.sh
-wget http://download.cirros-cloud.net/0.5.2/cirros-0.5.2-aarch64-disk.img -P ~
-openstack image create Cirros-CLI --file ~/cirros-0.5.2-aarch64-disk.img --disk-format raw --container-format bare --private
-openstack flavor create --public type.1C_1G --id auto --ram 0 --disk 0 --vcpus 1
+wget http://download.cirros-cloud.net/0.5.2/cirros-0.5.2-x86_64-disk.img -P ~
+openstack image create Cirros --file ~/cirros-0.5.2-x86_64-disk.img --disk-format qcow2 --shared
+openstack flavor create --public type.1C_1G --id auto --ram 1024 --disk 1 --vcpus 1
 openstack network create  --share --external --provider-physical-network physnet1 --provider-network-type flat provider1
 openstack subnet create --network provider1 --gateway 192.168.1.1 --subnet-range 192.168.1.1/24  subnet1
 openstack network create  --share --external --provider-physical-network physnet2 --provider-network-type flat provider2
@@ -13,4 +13,4 @@ openstack network create private
 openstack subnet create privsubnet --network private --subnet-range 192.0.123.0/24
 openstack router create router1 
 openstack router set router1 --external-gateway provider1
-openstack router add subnet privsubnet router1
+openstack router add subnet router1 privsubnet
