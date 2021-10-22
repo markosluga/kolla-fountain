46d5a52227b2d9559fa4ca1dcb392b6ea71c2f6f
OS <marko@markocloud.com>
2021-10-22 03:00:05 +0000

    added pv/vg commands

--- host-prep.sh
+++ host-prep.sh
@@ -4,3 +4,5 @@ sudo sed -i -e '$akolla ALL=(ALL) NOPASSWD: ALL' /etc/sudoers
 # Change the hosts entry to match to the kolla_internal_vip_address 
 sudo sed -i '/127.0.0.1 localhost/d' /etc/hosts
 sudo sed -i '/127.0.0.1 singlet/d' /etc/hosts
+sudo pvcreate /dev/sdb
+sudo vgcreate cinder-volumes /dev/sdb
