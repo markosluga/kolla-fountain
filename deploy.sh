d00d405ebc760ba75665cc80514bda383bb25fcb
markosluga <35494371+markosluga@users.noreply.github.com>
2021-10-22 13:59:51 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,7 @@ echo "Prepping host"
 source host-prep.sh
 echo "Installing dependecies and configureing kolla"
 source kolla-shaker.sh
-echo "Configuring kolla"
-source kolla-config.sh
-echo "Deploying kolla-single host"
+echo "Configuring and deploying kolla-single host"
 source kolla-maker.sh
 echo "Running post install"
 source kolla-post-install.sh
