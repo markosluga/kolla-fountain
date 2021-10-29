5ca73ecc609cbc6a4839b37ec9b9dea584e32d1e
markosluga <35494371+markosluga@users.noreply.github.com>
2021-10-29 11:20:11 -0400

    Update kolla-shaker-single.sh

--- kolla-shaker-single.sh
+++ kolla-shaker-single.sh
@@ -20,7 +20,7 @@ sudo sed -i "11i host_key_checking=False" /etc/ansible/ansible.cfg
 sudo sed -i "12i pipelining=True" /etc/ansible/ansible.cfg
 sudo sed -i "13i forks=100" /etc/ansible/ansible.cfg
 # Set up kolla all-in-one 
-sed -i '4s/$/localhost	ansible_connection=local ansible_user=kolla ansible_become=true/' ~/all-in-one
-sed -i '19s/$/localhost       ansible_connection=local become=true/' ~/all-in-one
+sed -i '4s/$/ ansible_user=kolla ansible_become=true/' ~/all-in-one
+sed -i '19s/$/ become=true/' ~/all-in-one
 # Ensure python docker module is imported
 python3 -c "import docker"
