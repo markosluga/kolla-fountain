558dfabc3f1d614cfa4920109f4e0fa78aa68918
markosluga <35494371+markosluga@users.noreply.github.com>
2021-10-29 11:12:09 -0400

    Update and rename deploy.sh to deploy-single.sh

--- deploy-single.sh
+++ deploy-single.sh
@@ -2,9 +2,11 @@
 echo "Prepping host"
 source host-prep.sh
 echo "Installing dependecies and configureing kolla"
-source kolla-shaker.sh
+source kolla-shaker-signle.sh
 echo "Configuring and deploying kolla-single host"
 source kolla-maker.sh
+echo "Depoloying single host"
+source kolla-deploy-single.sh
 echo "Running post install"
 source kolla-post-install.sh
 
