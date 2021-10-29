dbf2145c24cc5c750a1ccda37a99b20f76a95cf9
markosluga <35494371+markosluga@users.noreply.github.com>
2021-10-29 13:27:45 -0400

    Update host-prep.sh

--- host-prep.sh
+++ host-prep.sh
@@ -6,7 +6,6 @@ sudo sed -i '1,2d' /etc/hosts
 # Create an empty logical volume group for cinder
 sudo pvcreate /dev/sdb
 sudo vgcreate cinder-volumes /dev/sdb
-mkdir ~/.ssh
 cp home/kolla/.ssh/* ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
 chmod 400 ~/.ssh/id_rsa
