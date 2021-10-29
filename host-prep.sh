e93d98cafc9dcf6e513e6ed20f9a8715b1e211f6
markosluga <35494371+markosluga@users.noreply.github.com>
2021-10-29 13:03:46 -0400

    Update host-prep.sh

--- host-prep.sh
+++ host-prep.sh
@@ -6,9 +6,3 @@ sudo sed -i '1,2d' /etc/hosts
 # Create an empty logical volume group for cinder
 sudo pvcreate /dev/sdb
 sudo vgcreate cinder-volumes /dev/sdb
-# Set static DNS
-sudo systemctl stop systemd-resolved
-sudo systemctl disable systemd-resolved
-sudo sed -i '17,19d' /etc/resolv.conf
-sudo sed -i '16isearch lan' >> /etc/resolv.conf
-sudo sed -i '16inameserver 192.168.1.1' >> /etc/resolv.conf
