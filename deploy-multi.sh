9b323f6c46906ca65f669a8195cb06dcc3a6a684
markosluga <35494371+markosluga@users.noreply.github.com>
2021-11-25 10:20:04 -0500

    Update deploy-multi.sh

--- deploy-multi.sh
+++ deploy-multi.sh
@@ -3,9 +3,9 @@ echo "Prepping host"
 source ~/kolla-fountain/host-prep.sh
 echo "Installing dependecies and configureing kolla"
 source ~/kolla-fountain/kolla-shaker-multi.sh
-echo "Configuring and deploying kolla-single host"
-source ~/kolla-fountain/kolla-maker.sh
-echo "Depoloying single host"
+#echo "Configuring and deploying kolla-single host"
+#source ~/kolla-fountain/kolla-maker.sh
+echo "Depoloying multi host"
 source ~/kolla-fountain/kolla-deploy-multi.sh
 echo "Running post install"
 source ~/kolla-fountain/kolla-post-install.sh
