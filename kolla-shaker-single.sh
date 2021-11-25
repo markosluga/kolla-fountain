7c9f4cbaa6424312a474fc06f9e9d1098ce23087
markosluga <35494371+markosluga@users.noreply.github.com>
2021-11-25 10:18:35 -0500

    Update kolla-shaker-single.sh

--- kolla-shaker-single.sh
+++ kolla-shaker-single.sh
@@ -1,6 +1,5 @@
 #/bin/bash
 # Install prerequisites and kolla-ansible
-# Create kolla-shaker.sh and add the following contents to the file:
 sudo apt update
 sudo apt install -y docker.io python3-dev libffi-dev gcc libssl-dev sudo python3-pip
 sudo pip3 install -U pip
@@ -8,19 +7,28 @@ sudo pip3 install -U ansible==2.10
 sudo apt install -y ansible
 sudo pip3 install -U docker
 sudo pip3 install -U kolla-ansible
+
+# configure kolla etc files
 sudo mkdir -p /etc/kolla
 sudo chown $USER:$USER /etc/kolla
 cp -r /usr/local/share/kolla-ansible/etc_examples/kolla/* /etc/kolla
+
+#copy the globals.yml file from the sample in the home directory
+cp ~/kolla-fountain/etc/kolla/globals.yml /etc/kolla/globals.yml
 cp /usr/local/share/kolla-ansible/ansible/inventory/* ~
+
+#generate passwords
 kolla-genpwd
+
+# Create folders and copy neutron ml2_conf.ini
 sudo mkdir /etc/kolla/config/
 sudo mkdir /etc/kolla/config/neutron
 sudo chown $USER:$USER /etc/kolla/config
 sudo chown $USER:$USER /etc/kolla/config/neutron/
+cp ~/kolla-fountain/etc/kolla/neutron/ml2_conf.ini /etc/kolla/config/neutron/ml2_conf.ini
+
 # Set up ansible for kolla
-sudo sed -i "11i host_key_checking=False" /etc/ansible/ansible.cfg
-sudo sed -i "12i pipelining=True" /etc/ansible/ansible.cfg
-sudo sed -i "13i forks=100" /etc/ansible/ansible.cfg
+sudo cp ~/kolla-fountain/etc/ansible/ansible.cfg /etc/ansible/ansible.cfg
+
 # Set up kolla all-in-one 
-sed -i '4s/$/ ansible_user=kolla ansible_become=true/' ~/all-in-one
-sed -i '19s/$/ become=true/' ~/all-in-one
+cp ~/kolla-fountain/home/kolla/all-in-one ~/all-in-one
