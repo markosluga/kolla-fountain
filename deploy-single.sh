565b76b8df6ec2007aca91b7ba386827ac048e9e
markosluga <35494371+markosluga@users.noreply.github.com>
2021-11-25 10:30:42 -0500

    Update deploy-single.sh

--- deploy-single.sh
+++ deploy-single.sh
@@ -3,8 +3,6 @@ echo "Prepping host"
 source ~/kolla-fountain/host-prep.sh
 echo "Installing dependecies and configureing kolla"
 source ~/kolla-fountain/kolla-shaker-single.sh
-#echo "Configuring and deploying kolla-single host"
-#source ~/kolla-fountain/kolla-maker.sh
 echo "Depoloying single host"
 source ~/kolla-fountain/kolla-deploy-single.sh
 echo "Running post install"
