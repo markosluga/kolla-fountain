2dc2c3848b28c7668b7aa96d567bee3a45f8a407
markosluga <35494371+markosluga@users.noreply.github.com>
2021-11-26 09:53:13 -0500

    Update host-prep.sh

--- host-prep.sh
+++ host-prep.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Make kolla a sudoer
 sudo sed -i -e '$akolla ALL=(ALL) NOPASSWD: ALL' /etc/sudoers
-# Remove the hosts entries createed dby default (or else RabbitMQ fails)
-sudo sed -i '1,2d' /etc/hosts
+# Remove the hosts entries created by default (or else RabbitMQ fails)
+sudo cp ~/kolla-fountain/etc/hosts /etc/kolla/hosts
 # Create an empty logical volume group for cinder
 sudo pvcreate /dev/sdb
 sudo vgcreate cinder-volumes /dev/sdb
