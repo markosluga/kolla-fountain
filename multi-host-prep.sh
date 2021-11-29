2f4f8f313d3e0ab44f3752fc90a50da05046f1b5
markosluga <35494371+markosluga@users.noreply.github.com>
2021-11-29 12:33:30 -0500

    Update multi-host-prep.sh

--- multi-host-prep.sh
+++ multi-host-prep.sh
@@ -7,3 +7,10 @@ sudo cp ~/kolla-fountain/etc/hosts /etc/hosts
 cp ~/kolla-fountain/home/kolla/.ssh/* ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
 chmod 400 ~/.ssh/id_rsa
+sudo apt update
+sudo apt install -y docker.io python3-dev libffi-dev gcc libssl-dev sudo python3-pip
+sudo pip3 install -U pip
+sudo pip3 install -U ansible==2.10
+sudo apt install -y ansible
+sudo pip3 install -U docker
+sudo pip3 install -U kolla-ansible
