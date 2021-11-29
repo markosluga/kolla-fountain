9ea534962933029f096195cd0b4aae2aa1b5c4bd
markosluga <35494371+markosluga@users.noreply.github.com>
2021-11-29 11:31:20 -0500

    Update multi-host-prep.sh

--- multi-host-prep.sh
+++ multi-host-prep.sh
@@ -3,9 +3,6 @@
 sudo sed -i -e '$akolla ALL=(ALL) NOPASSWD: ALL' /etc/sudoers
 # Remove the hosts entries createed dby default (or else RabbitMQ fails)
 sudo cp ~/kolla-fountain/etc/hosts /etc/hosts
-# Create an empty logical volume group for cinder
-sudo pvcreate /dev/sdb
-sudo vgcreate cinder-volumes /dev/sdb
 # copy your ssh files to kolla .ssh directory
 cp ~/kolla-fountain/home/kolla/.ssh/* ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
